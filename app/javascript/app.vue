6773c51826b85d3b39bf065aed430421bcbf6066
github username <github email address>
2019-04-28 20:04:20 -0400

    cards can be added to list via vue, ajax and rails

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,20 +1,51 @@
 <template>
   <div id="app" class="row">
-   <div v-for="list in original_lists" class="col-3">
+   <div v-for="(list, index) in original_lists" class="col-3">
      <h6>{{ list.name }}</h6>
      <hr />
      <div v-for="(card, index) in list.cards" class="card card-body">
        {{ card.name }}
      </div>
-   </div>  
 
+     <div class="card card-body">
+      <textarea v-model="messages[list.id]" class="form-control"></textarea>
+      <button v-on:click="submitMessages(list.id)" class="btn">Add</button>
+    </div>
+
+   </div>  
   </div>
 </template>
 
 <script>
 export default {
   props: ["original_lists"],
-}
+  data: function(){
+    return {
+      messages: {},
+        lists: this.original_lists
+      }
+    },
+
+    methods: {
+      submitMessages: function(list_id) {
+        var data = new FormData 
+        data.append("card[list_id]", list_id)
+        data.append("card[name]", this.messages[list_id])
+
+        Rails.ajax({
+          url: "/cards",
+          type: "POST",
+          data: data, 
+          dataType: "json", 
+          success: (data) => {
+            const index = this.lists.findIndex(item => item.id == list_id) 
+            this.lists[index].cards.push(data)
+            this.messages[list_id] = undefined 
+          }
+        })
+      }
+    }
+  }
 </script>
 
 <style scoped>
